270bc4ab97a7ec17f7f9271f5b0b918001f63124
Dev-Salem <108498811+Dev-Salem@users.noreply.github.com>
2024-12-15 14:56:59 +0300

    Initialize Blog API project with basic structure and configuration

---
+++ Blog.Api/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
